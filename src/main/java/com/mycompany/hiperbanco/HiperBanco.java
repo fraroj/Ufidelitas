e9235fd8eb25d999126bbb2cc5e6d5f541489eea
fraroj <franciab443@gmail.com>
2025-06-21 11:29:26 -0600

    Added project files

---
+++ src/main/java/com/mycompany/hiperbanco/HiperBanco.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.hiperbanco;
+
+/**
+ *
+ * @author franciabrenesrojas
+ */
+public class HiperBanco {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
